0d7974b98dbb584edd1369e1ab905046f2ee4dc1
Aruna-Subramani <arunaece365@gmail.com>
2024-10-29 15:36:06 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_Fjq0eGbnS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ibub0hjo165prut69m828e6c4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
